7a122a9eead39967ba191b9e9f29469d7f5039fd
Rodrigo Luna <irodrigoro@gmail.com>
2023-07-02 01:01:24 -0600

    Requirments and src files

---
+++ src/daqmx_test.py
@@ -0,0 +1,11 @@
+# Performs a daqmx device test.
+# Analog output and input, Digital I/O or Counter I/O.
+
+import nidaqmx
+import numpy as np
+
+# Analog output test
+def ao_test(device_name, channel_name, voltage):
+    with nidaqmx.Task() as task:
+        task.ao_channels.add_ao_voltage_chan(device_name + '/' + channel_name)
+        task.write(voltage, auto_start=True)
---
+++ src/devices.py
@@ -0,0 +1,2 @@
+# Retrive modular instruments and daqmx devices names, and its properties 
+# from the system (channels, type of task, etc).

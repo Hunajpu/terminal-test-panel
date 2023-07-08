6f3a66d42114bbd0f365a6ab6e1499bcbd24eaef
Rodrigo Luna <rodrigo.luna@ni.com>
2023-07-07 23:04:23 -0600

    AI backbone

--- src/daqmx_test.py
+++ src/daqmx_test.py
@@ -9,3 +9,7 @@ def ao_test(device_name, channel_name, voltage):
     with nidaqmx.Task() as task:
         task.ao_channels.add_ao_voltage_chan(device_name + '/' + channel_name)
         task.write(voltage, auto_start=True)
+
+def ai_test(device_name, channel_name):
+    with nidaqmx.Task() as task:
+        task.ai_channels.add_ai_voltage_chan(device_name + '/' + channel_name)
